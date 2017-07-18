9f4f2ec4721611097636718f2ecf8af2a108f56c
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-07-18 10:19:46 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+class Keypad extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  enteringPassword(){
+    console.log('Entering password...')
+  }
+
+  render(){
+    return(
+      <input onKeyUp={this.enteringPassword} type="password" placeholder="password"/>
+    )
+  }
+}
+
+export default Keypad;
